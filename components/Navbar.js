82db05b0f700e3f3a9b80316fddc263c61c5598c
suron <suron.maharjan@gmail.com>
2023-01-25 20:47:50 +0545

    Navbar completed without logo

---
+++ components/Navbar.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className=" mx-16 px-8 py-6 rounded-b-xl bg-offWhite flex justify-between items-center font-bold text-my-green text-xs">
+      <ul className="flex gap-12">
+        <Link href={"/"}>
+          <li>How Apeel Works</li>
+        </Link>
+        <Link href={"/"}>
+          <li>Impact</li>
+        </Link>
+        <Link href={"/"}>
+          <li>Retail</li>
+        </Link>
+      </ul>
+
+      <ul className="flex gap-12">
+      <Link href={"/"}>
+          <li>Careers</li>
+        </Link>
+        <Link href={"/"}>
+          <li>Products</li>
+        </Link>
+        <Link href={"/"}>
+            <button>Find Apeel</button>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
@@ -15,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-       
+       <Navbar />
       </main>
     </>
   )
