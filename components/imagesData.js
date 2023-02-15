5ae44404ef8863d051b089fcd8f61ac44fff5bdd
suron <suron.maharjan@gmail.com>
2023-02-15 13:36:42 +0545

    [dummy data added]

--- components/imagesData.js
+++ components/imagesData.js
@@ -1,4 +1,21 @@
 
 export default {
-
-}
+  "success": "true",
+  "data": [
+    {
+      "name": "Avocados",
+      "country": "North America & Europe",
+      "src": "images/filters_quality(85) (3).webp",
+    },
+    {
+      "name": "English Cucumbers",
+      "country": "North America",
+      "src": "images/filters_quality(85) (3).webp",
+    },
+    {
+      "name": "Limes",
+      "country": "North America & Europe",
+      "src": "images/filters_quality(85) (3).webp",
+    },
+  ],
+};
